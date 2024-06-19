b296c2ce2c3ee3bac66a7c3e7ce762bee9a3a471
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-06-19 23:19:24 +0530

    feat: view all tasks in root index

--- app.py
+++ app.py
@@ -3,15 +3,23 @@ Flask app
 '''
 
 from flask import Flask, render_template
+from taskmanager import Task, TaskManager
 
 app = Flask(__name__)
+task_manager = TaskManager()
 
 @app.route('/')
 def index():
     '''
     Index page
     '''
-    return render_template('index.html')
+    task = Task('title', 'description', 'due_date', 'status')
+    task2 = Task('title1', 'description1', 'due_date1', 'status1')
+    task_manager.add_task(task)
+    task_manager.add_task(task2)
+    tasks = task_manager.view_tasks()
+    print(tasks[0])
+    return render_template('index.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
