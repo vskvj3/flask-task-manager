79b584d27d5b9544df0358494e82f6878bb7e1a6
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-06-19 21:32:44 +0530

    add task class

---
+++ taskmanager.py
@@ -0,0 +1,20 @@
+'''
+Models for the Task Manager application
+Task: Represents a task in the task manager application
+TaskManager: Functions to manage tasks
+'''
+import json
+from datetime import datetime
+
+class Task:
+    '''
+    Represents a task in the task manager application
+    '''
+    def __init__(self, title, description, due_date, status):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.status = status
+
+    def __str__(self):
+        return f"Title: {self.title}, Description: {self.description}, Due Date: {self.due_date}, Status: {self.status}"
